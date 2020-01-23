80a97e38a327751f8887eda3f3fb625555064d27
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2020-01-22 21:44:25 -0500

    feat: implement the line counting logic

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const fs = require('fs');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  const { exec } = require('child_process');
+  exec('git --no-pager show --pretty="" --name-only HEAD', (err, output, stderr) => {
+    if (err) {
+      console.log('\x1b[33m%s\x1b[0m', 'Could not find any path because: ');
+      console.log('\x1b[31m%s\x1b[0m', stderr);
+      process.exit(1);
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+      return;
+    }
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
+    console.log(`Output: ${output}`);
+
+    const paths = output.split('\n');
+    const path = paths.filter((val, idx, arr) => {
+      if (val.endsWith('.md')) {
+        return true;
+      }
+    });
+
+    console.log('\x1b[32m%s\x1b[0m', `Found: ${path}`);
+
+    exec(`wc -l ${path}`, (err, line, stderr) => {
+      const space_index = line.search(' ');
+      const line_count = parseInt(line.substr(0, space_index));
+      console.log(`::set-output name=linecount::${line_count}`);
+
+      if (line_count < 5) {
+        console.log('\x1b[33m%s\x1b[0m', 'File contain less than 5 lines.');
+        process.exit(1);
+      }
+      process.exit(0);
+    });
+  });
 }
 
 run()
